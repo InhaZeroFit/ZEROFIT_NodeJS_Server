22f3f4fcb24b289acc76696c49c95ef916e6dfc4
logicallaw <logicallawbio@gmail.com>
2024-11-15 12:22:49 +0900

    feat: Flutter /my-image 구현

--- routes/main.js
+++ routes/main.js
@@ -2,15 +2,21 @@ const express = require("express");
 const { home } = require("../controllers/main");
 const router = express.Router();
 
+const FLASK_PORT = 5005;
+
 router.get("/", home);
 
-router.get("/data", (req, res) => {
-    res.json({ message : "Hello from Node.js!"});
-});
-
-router.post("/data", (req, res) => {
-    console.log(req.body);
-    res.sendStatus(200);
+router.get("/my-image", async (req, res) => {
+    try {
+        const response = await fetch(`http://localhost:${FLASK_PORT}/user-image`);
+        const data = await response.json(); // Parsing in JSON format
+        res.json({
+            message : data.message,
+        });
+    } catch (error) {
+        console.error(`Flask 서버 요청 싪패: ${error}`);
+        res.status(500).send("Flask 서버 요청 실패");
+    };
 });
 
 module.exports = router;
