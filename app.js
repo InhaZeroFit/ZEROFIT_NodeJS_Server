5c9a84e62d7bee7b34a552bf564113d6824f8d9c
logicallaw <logicallawbio@gmail.com>
2024-11-15 11:30:04 +0900

    Refactor: Refactor the codes for readability

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express=require("express");
-
-const path = require("path")
-const cookie_parser = require("cookie-parser");
-const morgan = require("morgan");
-const session = require("express-session");
 const dotenv = require("dotenv");
 const nunjucks = require("nunjucks");
+const path = require("path")
+const morgan = require("morgan");
+const cookie_parser = require("cookie-parser");
+const session = require("express-session");
 const cors = require("cors");
-
 
 dotenv.config();
 
@@ -17,6 +15,7 @@ const db = require("./models");
 const app = express();
 const PORT = 8005;
 
+// Server setting
 app.set("port", PORT || process.env.PORT);
 app.set("view engine", "html");
 nunjucks.configure("views", {
@@ -24,6 +23,7 @@ nunjucks.configure("views", {
     watch : true,
 });
 
+// Database setting
 db.sequelize.sync({force : false})
     .then(() => {
         console.log("[ZEROFIT] Database & tables setted!");
@@ -35,9 +35,6 @@ db.sequelize.sync({force : false})
 // Middlewares
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
-
-app.use(cors()); // Allow another ports.
-
 app.use(express.json());
 app.use(express.urlencoded({extended:false}))
 app.use(cookie_parser(process.env.COOKIE_SECRET));
@@ -50,21 +47,23 @@ app.use(session({
         secure : false,
     }
 }));
+app.use(cors()); // Allow another ports.
 
 // Routes
 app.use("/", main_router);
 
+// Middlewares for error handing
 app.use((req, res, next) => {
     const error = new Error(`Not existed ${req.method} ${req.url} routes.`);
     error.status = 404;
     next(error);
-})
+});
 
 app.use((err,req,res,next) => {
     res.locals.message=err.message
-    res.locals.error=process.env.NODE_ENV !== 'production' ? err : {}
+    res.locals.error=process.env.NODE_ENV !== 'production' ? err : {} // If NODE_ENV isn't production then, print err
     res.status(err.status || 500)
     res.render('error')
-})
+});
 
 module.exports = app;
