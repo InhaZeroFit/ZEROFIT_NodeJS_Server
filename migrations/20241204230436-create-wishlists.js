a0c8719c24eb885b1d72b654aa40a5b1a2b22377
logicallaw <logicallawbio@gmail.com>
2024-12-16 15:50:34 +0900

    fix: migrations 파일 복원

---
+++ migrations/20241204230436-create-wishlists.js
@@ -0,0 +1,61 @@
+/*
+ * This is file of the project ZEROFIT_NODEJS_SERVER
+ * Licensed under the MIT License.
+ * Copyright (c) 2024 ZEROFIT_NODEJS_SERVER
+ * For full license text, see the LICENSE file in the root directory or at
+ * https://opensource.org/license/mit
+ * Author: logicallaw
+ * Latest Updated Date: 2024-12-16
+ */
+
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {    
+    await queryInterface.createTable('wishlists', {
+      wishlist_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'users', // users 테이블 참조
+          key: 'user_id',
+        },
+        onDelete: 'CASCADE',
+      },
+      clothes_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'clothes', // clothes 테이블 참조
+          key: 'clothes_id',
+        },
+        onDelete: 'CASCADE',
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn('NOW'),
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn('NOW'),
+      },
+      deleted_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('wishlists');
+  },
+}
+;
