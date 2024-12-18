bf87f7fcf028b6d0e5d0b52dfd034a4e0756438e
logicallaw <logicallawbio@gmail.com>
2024-12-18 22:10:20 +0900

    fix: /wishlist/info GET을 POST로 변경

--- routes/wishlist.js
+++ routes/wishlist.js
@@ -14,7 +14,7 @@ const jwt_middleware = require('../middlewares/jwt_middleware');
 const {add_to_wishlist, get_wishlist} = require('../controllers/wishlist');
 
 // GET /wishlist/info
-router.get('/info', jwt_middleware, get_wishlist);
+router.post('/info', jwt_middleware, get_wishlist);
 
 // POST /wishlist/add
 router.post('/add', jwt_middleware, add_to_wishlist);
